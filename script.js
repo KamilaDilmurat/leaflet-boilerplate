bd2b35b223e6a3b3f16cc0e3326d6748ad9ee0f8
KamilaDilmurat <89537254+KamilaDilmurat@users.noreply.github.com>
2022-02-08 23:08:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-
-var map = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+var map = L.map('mapid').setView([40,-75], 13);
+ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
+	minZoom: 0,
+	maxZoom: 10,
+	ext: 'png'
 }).addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
 
+var marker1 = L.marker([40, -75]).addTo(map)
+var marker2 = L.marker([40, -75.5]).addTo(map)
+var marker3 = L.marker([40, -75.3]).addTo(map)
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'orange',
-    fillColor: '#000099',
+var circle = L.circle([40, -75.2], {
+    color: 'red',
+    fillColor: '#f03',
     fillOpacity: 0.9,
-    radius: 500
+    radius: 5000
 }).addTo(map);
 
 var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-],{color:'red',fillColor:'blue'}).addTo(map)
+    [40, -75],
+    [40.2, -75.1],
+    [40.2, -75.2]
+],{color:'blue',
+   fillColor:'red'}).addTo(map);
 
-
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker1.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker2.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker3.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
+
+/* this is comment 
+var popup = L.popup()
+    .setLatLng([40, -75])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+*/
 
 function onMapClick(e) {
     alert("You clicked the map at " + e.latlng);
